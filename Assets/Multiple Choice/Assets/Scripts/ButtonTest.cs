34e1b80dea5e9801229386a9f5628a06a17dad05
Diego Castro <dcastro2@trinity.edu>
2014-12-06 16:02:12 -0600

    Literally the best game
    Pushed up Multiple Choice scenes, assets, and scripts and shit

---
+++ Assets/Multiple Choice/Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonTest : MonoBehaviour {
+	public Slider Slider;
+	public Button Button;
+	public bool rightAnswer;
+	//public GUIText guiText;
+	public Text hintTextA;
+	public Text hintTextB;
+	public Text hinttextC;
+	public Text hinttextD;
+
+	//public Tex
+	public int score = 0;
+
+	//Must be public. Must be void. Must have 0 to 1 parameters.
+	public void DoSomething() {
+		Debug.Log (Slider.value.ToString ());
+	}
+
+	public void DoSomethingWithASlider (Slider slider) {
+		Debug.Log (slider.value.ToString ());
+	}
+	
+	public void LoadScene(int level) {
+		Application.LoadLevel (level);
+	}
+
+	public void RightAnswer(Button button) {
+		score = score + 30;
+		Debug.Log ("Correct! Score = " + score);
+	}
+
+	public void WrongAnswer(Button button) {
+		score = score - 25;
+		Debug.Log ("Score = " + score );
+		//guiText.text = "This is okay";
+	}
+
+	public void DisplayHint(string msg) {
+		hintTextA.text = msg;
+		StartCoroutine (TimeDelay()); 
+		//hintTextA.text = "";
+	}
+
+	public IEnumerator TimeDelay (int seconds) {
+		yield return new WaitForSeconds(seconds);
+		hintTextA.text = "";
+	}
+
+//	void Awake() {
+//		DontDestroyOnLoad(score);
+//	}
+}
---
+++ Assets/Multiple Choice/Assets/Scripts/InputFieldEdit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InputFieldEdit : MonoBehaviour {
+
+	InputField inputField;
+	public void OnInputFieldEdit() {
+		inputField.guiText.ToString ();
+		Debug.Log ("the Text!");
+
+	}
+
+	public void Message() {
+		Debug.Log ("This is a test");
+
+	}
+
+	public void Message2() {
+		Debug.Log ("This is a test");
+		
+	}
+}
