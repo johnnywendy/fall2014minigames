bcb9448a7c9eb6baefadbc4535c711f0ce34ae11
c-wheeler <cwheele1@trinity.edu>
2014-12-08 23:47:01 -0600

    Game with the Worst Name
    Github isn't playing nice with my manager script, so I'm leaving it out
    on this version so we at least have a placeholder that compiles. Will
    commit the manager script once I find the conflict.

---
+++ Assets/Bitwisdom/Assets/Scripts/BitwiseInt.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Bitwise
+{
+	public class BitwiseInt {
+		
+		public int value,
+		size;
+		
+		public BitwiseInt(int value, int size){
+			this.value = value;
+			this.size = size;
+		}
+		
+		public BitwiseInt(int value)
+		{
+			this.value = value;
+		}
+		
+		public void Set (int value, int size){
+			this.value = value;
+			this.size = size;
+		}
+		
+		public void SetValue (int value){
+			this.value = value;
+		}
+		
+		public void SetSize (int size)
+		{
+			this.size = size;
+		}
+		
+		public static BitwiseInt operator & (BitwiseInt i1, BitwiseInt i2){
+			int x = (int)Mathf.Pow ((float)2,(float)i1.size-1);
+			int i = 0;
+			
+			while (x > 1) {
+				i = ((i1.value / x) == 1 && (i2.value / x) == 1) ? i + x : i;
+				i1.SetValue(i1.value % x);
+				i2.SetValue (i2.value % x);
+				x = x / 2;
+			}
+			
+			i = (i1.value == 1 && i2.value == 1) ? i + 1 : i;
+
+
+			return new BitwiseInt(i,i1.size);
+		}
+		
+		public static BitwiseInt operator | (BitwiseInt i1, BitwiseInt i2){
+			int x = (int)Mathf.Pow ((float)2,(float)i1.size-1);
+			int i = 0;
+			
+			while (x >= 1) {
+				i = ((i1.value / x) == 1 || (i2.value / x) == 1)? i+x:i;
+				i1.SetValue(i1.value % x);
+				i2.SetValue(i2.value % x);
+				x = x / 2;
+			}
+			
+			i = (i1.value == 1 || i2.value == 1) ? i + 1 : i;
+			
+			return new BitwiseInt(i,i1.size);
+		}
+		
+		public static BitwiseInt operator ^ (BitwiseInt i1, BitwiseInt i2){
+			int x = (int)Mathf.Pow ((float)2,(float)i1.size-1);
+			int i = 0;
+			
+			while (x >= 1) {
+				i = ((i1.value / x) == 1 || (i2.value / x) == 1)? i:i+x;
+				i1.SetValue(i1.value % x);
+				i2.SetValue(i2.value % x);
+				x = x / 2;
+			}
+			
+			i = (i1.value == 1 || i2.value == 1) ? i : i + 1;
+			
+			return new BitwiseInt(i,i1.size);
+		}
+		
+		public static BitwiseInt operator ~ (BitwiseInt i1){
+			int x = (int)Mathf.Pow ((float)2,(float)i1.size-1);
+			int i = 0;
+			
+			while (x >= 1) {
+				i = ((i1.value / x) == 1)? i:i+x;
+				i1.SetValue (i1.value % x);
+				x = x / 2;
+			}
+			
+			i = (i1.value == 1) ? i : i + 1;
+			
+			return new BitwiseInt(i,i1.size);
+		}
+	}
+}
