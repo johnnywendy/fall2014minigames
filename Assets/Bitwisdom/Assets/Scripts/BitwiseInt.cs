fece5475a7b1de7e44476b8f575e7f96c04b1d3d
c-wheeler <cwheele1@trinity.edu>
2014-12-10 15:01:12 -0600

    Feeling good on a Wednesday...

--- Assets/Bitwisdom/Assets/Scripts/BitwiseInt.cs
+++ Assets/Bitwisdom/Assets/Scripts/BitwiseInt.cs
@@ -53,7 +53,7 @@ namespace Bitwise
 			int x = (int)Mathf.Pow ((float)2,(float)i1.size-1);
 			int i = 0;
 			
-			while (x >= 1) {
+			while (x > 1) {
 				i = ((i1.value / x) == 1 || (i2.value / x) == 1)? i+x:i;
 				i1.SetValue(i1.value % x);
 				i2.SetValue(i2.value % x);
@@ -69,14 +69,14 @@ namespace Bitwise
 			int x = (int)Mathf.Pow ((float)2,(float)i1.size-1);
 			int i = 0;
 			
-			while (x >= 1) {
-				i = ((i1.value / x) == 1 || (i2.value / x) == 1)? i:i+x;
+			while (x > 1) {
+				i = (((i1.value / x) == 1 || (i2.value / x) == 1) && !((i1.value / x) == 1 && (i2.value / x))? i:i+x;
 				i1.SetValue(i1.value % x);
 				i2.SetValue(i2.value % x);
 				x = x / 2;
 			}
 			
-			i = (i1.value == 1 || i2.value == 1) ? i : i + 1;
+			i = ((i1.value == 1 || i2.value == 1) && !(i1.value == 1 && i2.value == 1)) ? i : i + 1;
 			
 			return new BitwiseInt(i,i1.size);
 		}
@@ -85,7 +85,7 @@ namespace Bitwise
 			int x = (int)Mathf.Pow ((float)2,(float)i1.size-1);
 			int i = 0;
 			
-			while (x >= 1) {
+			while (x > 1) {
 				i = ((i1.value / x) == 1)? i:i+x;
 				i1.SetValue (i1.value % x);
 				x = x / 2;
