ada033111629206bf27585629e0ec273ad1a4470
hmcdona1 <gmail@reecemcdonald.com>
2015-02-18 23:03:18 -0600

    Revert 9d7bd62..9fe474a
    This rolls back to commit 9d7bd62bf37029788d68af1b69841d6aa9705012.

--- Assets/Logic Gates/Scripts/LogicGate.cs
+++ Assets/Logic Gates/Scripts/LogicGate.cs
@@ -317,8 +317,12 @@ public class LogicGate : MonoBehaviour {
 		cables.Add(newCable);
 		newCable = null;
 	}
-	
+
 	public void UnplugFromGate(LogicGate gate) {
+		pluggedGates.RemoveAt(PrepUnplugFromGate(gate));
+	}
+
+	public int PrepUnplugFromGate(LogicGate gate) {
 		int index = pluggedGates.IndexOf(gate);
 		if (pluggedSides[index] == "right") {
 			pluggedGates[index].power2 = false;
@@ -330,7 +334,6 @@ public class LogicGate : MonoBehaviour {
 			pluggedGates[index].input1 = false;
 			pluggedGates[index].leftConnectedGate = null;
 		}
-		pluggedGates.RemoveAt(index);
 		pluggedSides.RemoveAt(index);
 		List<Cable> shouldRemove = new List<Cable>();
 		foreach (Cable cable in cables) {
@@ -345,6 +348,7 @@ public class LogicGate : MonoBehaviour {
 			cables.Remove(cable);
 			GameObject.Destroy(cable.gameObject);
 		}
+		return index;
 	}
 
 	public void powerReset(string side) {
@@ -365,8 +369,11 @@ public class LogicGate : MonoBehaviour {
 			leftConnectedPower.UnplugFromGate(this);
 		if (goalGate != null)
 			goalGate.resetConnection();
+		List<int> shouldRemoveGates = new List<int>();
 		foreach (LogicGate gate in pluggedGates)
-			UnplugFromGate(gate);
+			shouldRemoveGates.Add(PrepUnplugFromGate(gate));
+		foreach (int index in shouldRemoveGates)
+			pluggedGates.RemoveAt(index);
 		foreach (Cable acable in cables)
 			GameObject.Destroy(acable.gameObject);
 		cables = new List<Cable>();
