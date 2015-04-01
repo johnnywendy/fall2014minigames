783b34e32d9c6e1150fe66e95fd893e696543f91
hmcdona1 <gmail@reecemcdonald.com>
2015-04-01 18:25:42 -0500

    New Cipher Game Playable

--- Assets/Caesar Cipher/Scripts/CCManager.cs
+++ Assets/Caesar Cipher/Scripts/CCManager.cs
@@ -3,11 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class CCManager : MonoBehaviour {
-
-	public List<string> words = new List<string>() {"test"};
+	
+	private List<string> words = new List<string>() {"test","hello","world"};
 	public List<LetterBlock> encrypted;
 	public List<LetterBlock> decrypted;
 	public List<LetterBlock> options;
+	public TextMesh shiftAmount;
+	public GameObject heldObj;
+	private int _shiftAmount = 0;
+	private string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private string activeWord;
 	private int _activeIndex = 0;
 	public int activeIndex {
 		get {
@@ -17,22 +22,82 @@ public class CCManager : MonoBehaviour {
 			_activeIndex = value;
 			HexColor.SetColor(encrypted[encrypted.Count-1-_activeIndex].gameObject,GameColors.off);
 			HexColor.SetColor(decrypted[decrypted.Count-1-_activeIndex].gameObject,GameColors.on2);
+			if (_activeIndex > 0) {
+				if (_activeIndex < activeWord.Length) 
+					SetupNextLetter();
+				else
+					GameOver();
+			}
 		}
 	}
 
 	// Use this for initialization
 	void Start () {
-		foreach (LetterBlock block in encrypted) {
+		shiftAmount.color = HexColor.HexToColor(GameColors.inactive);
+		Random.seed = (int)System.DateTime.Now.Ticks;
+		foreach (LetterBlock block in options)
 			HexColor.SetColor(block.gameObject,GameColors.inactive);
-		}
-		foreach (LetterBlock block in decrypted) {
-			HexColor.SetColor(block.gameObject,GameColors.inactive2);
-		}
-		activeIndex = 0;
+		StartNewGame();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetMouseButtonUp(0)) {
+			if (heldObj != null && !heldObj.GetComponent<LetterBlock>().isHittingCurrent) { 
+				GameObject.Destroy(heldObj);
+				heldObj = null;
+			}
+			else {
+				heldObj.transform.localScale = heldObj.GetComponent<LetterBlock>().startScale;
+				IsCorrect(heldObj.GetComponent<LetterBlock>());
+			}
+		}
+	}
+
+	public void StartNewGame() {
+		foreach (LetterBlock block in encrypted)
+			HexColor.SetColor(block.gameObject,GameColors.inactive);
+		foreach (LetterBlock block in decrypted)
+			HexColor.SetColor(block.gameObject,GameColors.inactive2);
+		activeIndex = 0;
+		Random.seed = (int)System.DateTime.Now.Ticks;
+		activeWord = words[Random.Range(0,words.Count)].ToUpper();
+		_shiftAmount = ((int)Random.Range (-5, 5));
+		shiftAmount.text = (-_shiftAmount).ToString();
+		encrypted[encrypted.Count-1-activeIndex].SetText(ShiftChar(activeWord[activeIndex].ToString(),_shiftAmount));
+	}
+
+	void SetupNextLetter() {
+		_shiftAmount = ((int)Random.Range (-5, 5));
+		shiftAmount.text = (-_shiftAmount).ToString();
+		encrypted[encrypted.Count-1-activeIndex].SetText(ShiftChar(activeWord[activeIndex].ToString(),_shiftAmount));
+	}
+
+	void GameOver() {
+
+	}
+
+	string ShiftChar(string chr, int amount) {
+		int index = alphabet.IndexOf(((char)(chr.ToUpper()[0]+amount)).ToString());
+		if (index < 0) index += 26;
+		if (index > 26) index -= 26;
+		return alphabet[index].ToString();
+	}
+
+	void NextLetter() {
+
+	}
+
+	public void IsCorrect(LetterBlock block) {
+		if (block.isHittingCurrent && block.letter.text.ToUpper() == activeWord[activeIndex].ToString().ToUpper()) {
+			decrypted[decrypted.Count-1-activeIndex].GetComponent<LetterBlock>().SetText(block.letter.text);
+			HexColor.SetColor(decrypted[decrypted.Count-1-_activeIndex].gameObject,GameColors.on);
+			activeIndex++;
+		}
+		else {
+			HexColor.SetColor(decrypted[decrypted.Count-1-_activeIndex].gameObject,GameColors.off2);
+		}
+		GameObject.Destroy(block.gameObject);
+		heldObj = null;
 	}
 }
--- Assets/Caesar Cipher/Scripts/LetterBlock.cs
+++ Assets/Caesar Cipher/Scripts/LetterBlock.cs
@@ -3,22 +3,93 @@ using System.Collections;
 
 public class LetterBlock : MonoBehaviour {
 
-	private TextMesh letter;
+	public TextMesh letter;
 	public string startText = "";
 	public bool generator = false;
+	public bool isHeld = true;
+	public bool isStatic;
+	public bool isHittingCurrent = false;
+
+	public Vector3 startScale;
+	private Vector3 heldScale;
+	private CCManager gm;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+		startScale = transform.localScale;
+		heldScale = new Vector3 (startScale.x*1.1f,startScale.y*1.1f,startScale.z);
 		letter = GetComponentsInChildren<TextMesh>()[0];
 		letter.text = startText;
+		gm = Camera.main.GetComponent<CCManager>();
+		if (isStatic) isHeld = false; 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		if (isHeld) {
+			transform.localScale = heldScale;
+			transform.position = VisibleMousePosition();
+		}
+	}
+
+	public Vector3 VisibleMousePosition() {
+		Vector3 mousPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		return new Vector3 (mousPos.x, mousPos.y, -2f);
+	}
+
+	public Vector3 RayCheck() {
+		Vector3 mousPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		return new Vector3 (mousPos.x, mousPos.y, -1f);
 	}
 
 	public void SetText(string newText) {
 		letter.text = newText;
+	}
+
+	void OnMouseDown() {
+		if (generator) {
+			GameObject newBlock = (GameObject)Instantiate(Resources.Load("LetterBlock", typeof(GameObject)),Camera.main.ScreenToWorldPoint(Input.mousePosition),Quaternion.identity);
+			HexColor.SetColor(newBlock,GameColors.selected);
+			newBlock.GetComponent<LetterBlock>().generator = false;
+			newBlock.GetComponent<LetterBlock>().isHeld = true;
+			newBlock.GetComponent<LetterBlock>().SetText(letter.text);
+			newBlock.AddComponent<Rigidbody2D>();
+			newBlock.GetComponent<Rigidbody2D>().gravityScale = 0;
+			newBlock.GetComponent<Rigidbody2D>().fixedAngle = true;
+			newBlock.GetComponent<Rigidbody2D>().isKinematic = true;
+			gm.heldObj = newBlock;
+		}
+	}
+
+	void OnMouseEnter() {
+		if (generator)
+			HexColor.SetColor(this.gameObject,GameColors.on2);
+	}
+
+	void OnMouseExit() {
+		if (generator)
+			HexColor.SetColor(this.gameObject,GameColors.inactive);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (isHeld && !isHittingCurrent) {
+			if (other.gameObject == gm.decrypted[gm.decrypted.Count-1-gm.activeIndex].gameObject) {
+				HexColor.SetColor(this.gameObject,GameColors.on);
+				isHittingCurrent = true;
+			}
+			else {
+				HexColor.SetColor(this.gameObject,GameColors.off);
+				isHittingCurrent = false;
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject == gm.decrypted[gm.decrypted.Count-1-gm.activeIndex].gameObject) {
+			isHittingCurrent = false;
+		}
+		else if (isHeld) {
+			HexColor.SetColor(this.gameObject,GameColors.selected);
+		}
 	}
 }
