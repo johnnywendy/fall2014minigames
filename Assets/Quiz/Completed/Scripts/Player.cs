a4663f774efb84a396cdb7150787d07c477bfeac
hmcdona1 <gmail@reecemcdonald.com>
2015-04-27 12:32:34 -0500

    thing

--- Assets/Quiz/Completed/Scripts/Player.cs
+++ Assets/Quiz/Completed/Scripts/Player.cs
@@ -63,7 +63,7 @@ namespace Completed
 			int vertical = 0;		//Used to store the vertical move direction.
 			
 			//Check if we are running either in the Unity editor or in a standalone build.
-			#if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE || UNITY_STANDALONE || UNITY_WEBPLAYER
+			#if UNITY_STANDALONE || UNITY_WEBPLAYER
 			
 			//Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
 			horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
